9068b7351aeea46c254f9ca7f27f92f63dbea66a
dipin_yadav <dipin.yadav@shipway.in>
2023-06-14 06:49:47 +0530

    clock-example-1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// here we're directly rendering whole UI using root.render
+
+function tick() {
+	const element = (
+		<div>
+			<h1>Hello World!!!</h1>
+			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+		</div>
+	);
+
+	// root.render will update the UI every time tick is called.
+
+	root.render(element);
+}
+
+// setInterval will call function tick every second 
+
+setInterval(tick, 1000);
