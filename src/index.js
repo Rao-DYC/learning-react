0b4c14447948a4600c474ebbe2040236c78d24e4
dipin_yadav <dipin.yadav@shipway.in>
2023-06-14 08:52:29 +0530

    clock-example-2

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// here we're directly rendering whole UI using root.render
-
-function tick() {
-	const element = (
+// this time we converted Clock into a component of it's own
+function Clock({date}) {
+	return (
 		<div>
-			<h1>Hello World!!!</h1>
-			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+			<h1>Hello World</h1>
+			<h2>It is {date.toLocaleTimeString()}.</h2>
 		</div>
 	);
-
-	// root.render will update the UI every time tick is called.
-
-	root.render(element);
 }
 
-// setInterval will call function tick every second 
+// tick1 will call clock compnent and will render output on screen
+function tick1(){
+	root.render(<Clock date={new Date()}/>);
+}
 
-setInterval(tick, 1000);
+// setInterval will call tick1 every second
+setInterval(tick1, 1000);
